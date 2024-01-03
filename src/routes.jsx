78feb437220ce2f655496626dcf091d1949c9943
Kavishka Nimsara <nimsaragk.21@uom.lk>
2024-01-03 21:52:53 +0530

    setup routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,7 +6,7 @@ import {
   ServerStackIcon,
   RectangleStackIcon,
 } from "@heroicons/react/24/solid";
-import { Home, Profile, Tables, Notifications } from "@/pages/dashboard";
+import { MyProducts, MyOrders, NewOrders, MyReviews, MyReturns, Profile } from "@/pages/dashboard";
 import { SignIn, SignUp } from "@/pages/auth";
 
 const icon = {
@@ -15,43 +15,57 @@ const icon = {
 
 export const routes = [
   {
+    title: "Products",
     layout: "dashboard",
     pages: [
       {
         icon: <HomeIcon {...icon} />,
-        name: "dashboard",
-        path: "/home",
-        element: <Home />,
+        name: "My Products",
+        path: "/my-products",
+        element: <MyProducts />,
       },
+    ],
+  },
+  {
+
+    layout: "dashboard",
+    title: "orders",
+    pages: [
       {
         icon: <UserCircleIcon {...icon} />,
-        name: "profile",
-        path: "/profile",
-        element: <Profile />,
+        name: "New Orders",
+        path: "/new-orders",
+        element: <NewOrders />,
       },
       {
         icon: <TableCellsIcon {...icon} />,
-        name: "tables",
-        path: "/tables",
-        element: <Tables />,
+        name: "My Orders",
+        path: "/my-orders",
+        element: <MyOrders />,
       },
       {
         icon: <InformationCircleIcon {...icon} />,
-        name: "notifications",
-        path: "/notifications",
-        element: <Notifications />,
+        name: "my reviews",
+        path: "/my-reviews",
+        element: <MyReviews />,
+      },
+      {
+        icon: <InformationCircleIcon {...icon} />,
+        name: "my returns",
+        path: "/my-returns",
+        element: <MyReturns />,
       },
     ],
   },
   {
     title: "auth pages",
-    layout: "auth",
+    layout: "dashboard",
     pages: [
       {
         icon: <ServerStackIcon {...icon} />,
-        name: "sign in",
-        path: "/sign-in",
-        element: <SignIn />,
+        name: "Profile",
+        path: "/profile",
+        element: <Profile />,
       },
       {
         icon: <RectangleStackIcon {...icon} />,
