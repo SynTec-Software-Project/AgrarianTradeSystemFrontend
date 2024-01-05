b5c5864972f61c13341d45d453c5228ea8470b0c
Kavishka Nimsara <nimsaragk.21@uom.lk>
2024-01-05 20:47:34 +0530

    add a pharagraph

--- src/user/LandingPage.jsx
+++ src/user/LandingPage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div><h1>Hello this is landing page</h1></div>
+    <div><h1>Hello this is landing page
+      </h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae vel numquam qui dicta eaque! Sit
+         ipsa commodi quia enim expedita voluptas repudiandae maxime eligendi. Vel doloribus aut porro neque alias.</p>
+      </div>
   )
 }
 
