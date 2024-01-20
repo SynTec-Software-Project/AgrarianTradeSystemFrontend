20ba0b2fcf02afa88149de6c3af397dde410f611
Nimesha Abesinghe <abesinghenimesha20@gmail.com>
2024-01-20 14:12:49 +0530

    Added components folder and added tab.jsx inside components.  Called Tab componet to seller my-orders  Coded sales tabs and tables.  Added custom_gray color to tailwind.config.cjs  Added image to img folder.

---
+++ src/data/seller-table-data.js
@@ -0,0 +1,48 @@
+export const sellerTableData=[
+    {
+        
+        orderReference : "0000597899",
+        product : "Carrot",
+        orderPlaced : "2023-12-20",
+        quantity : 50,
+        courierId : 123,
+        photoName:"/img/carrot.png",
+        status: "readytopickup",
+        price: 2000
+    },
+    {
+        
+        orderReference : "0000497899",
+        product : "Beetroot",
+        orderPlaced : "2023-01-20",
+        quantity : 100,
+        courierId : 231,
+        photoName:"/img/carrot.png",
+        status: "readytopickup",
+        price: 3000
+    },
+    {
+        
+        orderReference : "0000697899",
+        product : "Carrot",
+        orderPlaced : "2023-11-20",
+        quantity : 70,
+        courierId : 426,
+        photoName:"/img/carrot.png",
+        status: "pickedup",
+        price: 4000
+    },
+    {
+        
+        orderReference : "0000897899",
+        product : "Beetroot",
+        orderPlaced : "2023-11-20",
+        quantity : 70,
+        courierId : 426,
+        photoName:"./../../public/img/carrot.png",
+        status: "delivered",
+        price: 2500
+    },
+];
+
+export default sellerTableData;
