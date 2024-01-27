503cb69fd2a7255172d3a91ee98898090951fc16
Kavishka Nimsara <nimsaragk.21@uom.lk>
2024-01-27 14:32:45 +0530

    Setup product form

--- src/pages/SellerDashboard/dashboard/forms/AddProductForm.jsx
+++ src/pages/SellerDashboard/dashboard/forms/AddProductForm.jsx
@@ -24,13 +24,16 @@ const AddProductForm = () => {
   const handleProductTypeChange = (event) => {
     const value = event.target.value;
     setSelectedProductType(value);
+    console.log(value);
 
     setSelectedCategory('');
   };
 
   const handleCategoryChange = (event) => {
-    const value = event.target.value;
-    setSelectedCategory(value);
+    if (event && event.target) {
+      setSelectedCategory(event.target.value);
+      console.log(event.target.value);
+    }
   };
 
   function addProducts() {
@@ -98,7 +101,7 @@ const AddProductForm = () => {
                 <Title title='Product Title' />
               </div>
               <input type="text" id="number-input" aria-describedby="helper-text-explanation"
-                class="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 placeholder="Title" required
                 ref={productTitleRef}
               >
@@ -110,73 +113,75 @@ const AddProductForm = () => {
               <Title title="Description"></Title>
               <div class="relative w-full min-w-[200px]">
                 <textarea
-                  class="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
+                  className="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
                   placeholder=" "
                   ref={productDescriptionRef}
                 ></textarea>
 
                 <label
-                  class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                  className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                   Description
                 </label>
               </div>
             </div>
 
-            {/* Category selection
-               <Title title="Select Category"></Title>
-                <div className="flex gap-10 -mt-3">
-                      <Radio name="type" 
-                      value="vegetable"
-                      checked={selectedProductType === 'vegetable'}
-                      onChange={handleProductTypeChange}
-                       label={
-                        <Typography
-                          color="blue-gray"
-                          className="font-normal text-blue-gray-700"
-                        >
-                          Vegitable
-                        </Typography>
-                      }
-                      defaultChecked
-                      />
-                    <Radio name="type"  
-                    value="fruit"
-                    checked={selectedProductType === 'fruit'}
-                    onChange={handleProductTypeChange}
-                    
-                    label={
-                        <Typography
-                          color="blue-gray"
-                          className="font-normal text-blue-gray-700"
-                        >
-                          Fruit
-                        </Typography>
-                      }
-                      />
-                   </div>
+            {/* select type  */}
+            <Title title="Select Category"></Title>
+            <div className="flex gap-10 -mt-3">
+              <Radio name="type"
+                value="vegetable"
+                checked={selectedProductType === 'vegetable'}
+                onChange={handleProductTypeChange}
+                label={
+                  <Typography
+                    color="blue-gray"
+                    className="font-normal text-blue-gray-700"
+                  >
+                    Vegitable
+                  </Typography>
+                }
+                defaultChecked
+              />
+              <Radio name="type"
+                value="fruit"
+                checked={selectedProductType === 'fruit'}
+                onChange={handleProductTypeChange}
 
-                  <div>
-                    <div className="w-full">
-                      <Select label="Select Type" value={selectedCategory} onChange={handleCategoryChange} >
+                label={
+                  <Typography
+                    color="blue-gray"
+                    className="font-normal text-blue-gray-700"
+                  >
+                    Fruit
+                  </Typography>
+                }
+              />
+            </div>
 
-                    
-          {selectedProductType === 'vegetable' ? (
-            <>
-                  {vegetables.map((v) => (
-                        <Option key={v.value} value={v.value}>{v.label}</Option>
-                    ))}
-            </>
-          ) : selectedProductType === 'fruit' ? (
-            <>
-              {fruits.map((f)=>{
-                           <Option key={f.value} value={f.value}>{f.label}</Option>
-                      })}
-            </>
-          ) : null}
-                                       
-                      </Select> 
-                  </div>      
-                </div> */}
+            {/* select category */}
+            <div className="w-full">
+              <select label={'Select '+selectedProductType} value={selectedCategory} onChange={handleCategoryChange}
+               className='overflow-y-scroll
+               h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 bg-gray-50 focus:ring-blue-500
+               block  p-2.5'
+              
+              >
+              
+               {selectedProductType === 'vegetable' ? (
+                    <>
+                      {vegetables.map((v) => (
+                        <option key={v.value} value={v.value}>{v.label}</option>
+                      ))}
+                    </>
+                  ) : selectedProductType === 'fruit' ? (
+                    <>
+                     {fruits.map((v) => (
+                        <option key={v.value} value={v.value}>{v.label}</option>
+                      ))}
+                    </>
+                  ) : null}
+              </select>
+            </div>
 
             {/* upload product image */}
             <Title title='Upload Product Image' />
@@ -199,8 +204,8 @@ const AddProductForm = () => {
                           <input id="dropzone-file" type="file" class="hidden" />
                  </label>
                  </div>  */}
-                 
-                 
+
+
             {/* select quantity */}
             <div>
               <div className=' flex'>
